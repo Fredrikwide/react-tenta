e4f7717c16949f38392da7fa0ac8be8f35594036
Fredrikwide <Fredrik.widegren@gmail.com>
2020-03-02 14:21:56 +0100

    changed icon and display options in weatherReport

--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -7,10 +7,14 @@ const WeatherReport = props => {
 
 		<div id="WeatherReport">
 			<div className="card bg-dark text-white">
-				<div className="card-body text-center">
+				<div className="card-body text-center" style={{
+				display: 'flex', 
+				justifyContent:'center',
+				alignItems: 'center',
+				flexDirection: 'column'}}>
 					<h5 className="card-title">The temperature in {props.city} is {props.temp} &deg; C right now, with a humidity of {props.humidity} %.</h5>
-					<h5>weather right now is: {props.description}</h5>
-					<span>{props.icon}</span>
+					<h5 style={{textAlign:'center', padding: '1rem'}}>weather right now is: <br />{props.description}</h5>
+					<img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`}/>
 				</div>
 			</div>
 		</div> 
